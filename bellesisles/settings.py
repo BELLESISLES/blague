5e79dddb943939b6bd60a1b86a5ea323ab91b2dc
BELLESISLES <lhasselin@bigfoot.com>
2016-11-21 22:38:01 +0100

    Modification du HTML du site

--- bellesisles/settings.py
+++ bellesisles/settings.py
@@ -104,10 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'fr-FR'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+1'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
